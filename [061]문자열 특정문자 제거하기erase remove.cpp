2c79741267e0fce4193abc24be4e58b2675b26f6
jposco <88217259+jposco@users.noreply.github.com>
2023-05-27 21:01:34 +0900

    Update [061]문자열 특정문자 제거하기erase remove.cpp

--- [061]문자열 특정문자 제거하기erase remove.cpp
+++ [061]문자열 특정문자 제거하기erase remove.cpp
@@ -15,3 +15,18 @@ int main()
 
 	return 0;
 };
+
+//모음 제거하기
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+string solution(string my_string) {
+    vector<char> v = {'a', 'e', 'i', 'o', 'u'};
+    for(auto i : v)
+    {
+        my_string.erase(remove(my_string.begin(),my_string.end(),i ),my_string.end());
+    }
+    return my_string;
+}
