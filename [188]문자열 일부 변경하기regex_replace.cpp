c501cda5a2a36aa7fb059627dcd7955bd711865a
jposco <88217259+jposco@users.noreply.github.com>
2023-06-03 13:15:53 +0900

    Update [188]문자열 일부 변경하기regex_replace.cpp

--- [188]문자열 일부 변경하기regex_replace.cpp
+++ [188]문자열 일부 변경하기regex_replace.cpp
@@ -21,6 +21,15 @@ int main()
 	cout << "result2 : " << result2 << endl;
 	cout << "result3 : " << result3 << endl;
 	cout << "result4 : " << result4 << endl;
+	
+	//re1: l, i, k, e 문자 중 하나를 발견하면 해당 문자를 [$&]로 치환합니다.
+	//re2: 숫자가 아닌(non-digit) 문자를 발견하면 _, 해당 문자, _로 치환합니다.
+	//re3: 공백 문자를 발견하면 (space)로 치환합니다.
+	
+	//result1: i [l][i][k][e] coding
+	//result2: i * * coding
+	//result3: i _i_ l_i_k_e coding
+	//result4: i(space)like(space)coding
 
 	return 0;
 }
